56381ed93105ddf2c8b0fa81e50fa230541b0626
Örkki Tuomi <Orkki.Tuomi@metropolia.fi>
2020-03-18 11:58:10 +0200

    Aloitustiimi minä

---
+++ Us.php
@@ -0,0 +1,19 @@
+<html lang="fi-FI">
+<head>
+      <title>Lab 21 </title>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" type="text/css" href="css/lab2.css">
+</head>
+
+<body>
+
+<p>
+<h2>Tiimimme "Leftovers" kolme jäsentä:</h2>
+<ol>
+    <li>Örkki Tuomi</li>
+    <li>Janina Saarinen</li>
+    <li>Niina Heinonen</li>
+</ol>
+<p>
+</body>
+</html>
